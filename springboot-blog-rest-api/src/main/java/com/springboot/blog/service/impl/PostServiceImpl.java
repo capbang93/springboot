3852ecc103ed7b491843cde0b47d7bef0268123b
capbang93 <capbang93@gmail.com>
2021-05-28 17:10:57 +0900

    Add files via upload

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,112 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+@Service
+//@Transactional is no needed because JPARepository aleady have defaul Transactional annotation
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    // if only 1 constructor @Autowired can be omitted
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        // convert DTO to entity
+        Post post = mapToEntity(postDto);
+        /*
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        */
+
+        Post newPost = postRepository.save(post);
+
+        // convert post entity to DTO
+        PostDto postResponse = mapToDTO(newPost);
+
+//        PostDto postResponse = new PostDto();
+//        postResponse.setId(newPost.getId());
+//        postResponse.setTitle(newPost.getTitle());
+//        postResponse.setDescription(newPost.getDescription());
+//        postResponse.setContent(newPost.getContent());
+
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+
+        // java api stream api
+        // we gonna use map
+        // we are just map post entity to DTO
+        // then collect the result
+        // The result is List
+        // service getallposts then postcontroller class moving
+        return posts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+       Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post","id",id));
+       return mapToDTO(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        // get post by id from the database
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post","id",id));
+
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatedPost = postRepository.save(post);
+        return mapToDTO(updatedPost);
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post","id",id));
+        postRepository.delete(post);
+    }
+
+    //convert Entity into DTO
+    private PostDto mapToDTO(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
+    }
+
+    // convert DTO to entity
+    private Post mapToEntity(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
+    }
+
+}
